43644f7531a020a14d78052a1c91d05a4b57b664
fuse <m13.f98@gmail.com>
2022-01-11 18:34:21 +0330

    Add integrate result script

---
+++ integrate_results.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+
+ID_COLUMN_TITLE = 'user_id'
+BLOCK_NUM_TITLE = 'block_num'
+
+FINAL_RESULT_FILENAME = 'all_results.csv'
+
+RES_PATH = './results' 
+
+os.chdir(RES_PATH)
+csv_filenames = os.listdir()
+def main():
+  all_dfs = []
+  for f in csv_filenames:
+    if 'csv' not in f or f == FINAL_RESULT_FILENAME:
+      continue
+    id_block, _ = f.split('.')
+    id, block = id_block.split('_')
+    df = pd.read_csv(f)
+    df[ID_COLUMN_TITLE] = int(id)
+    df[BLOCK_NUM_TITLE] = int(block)
+    all_dfs.append(df)
+
+  combined_results = pd.concat(all_dfs, sort=False)
+
+  combined_results.to_csv(FINAL_RESULT_FILENAME, index=False, encoding='utf-8-sig')
+
+if __name__ == '__main__':
+  main()
+
